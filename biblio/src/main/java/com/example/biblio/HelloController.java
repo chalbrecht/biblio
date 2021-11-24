d1f43929039ac092602196036c79a580f5bea2e0
EddyMuzaffar <eddy.muzaffar@outlook.com>
2021-11-24 16:23:47 +0100

    ajout de l'interface graphique

--- biblio/src/main/java/com/example/biblio/HelloController.java
+++ biblio/src/main/java/com/example/biblio/HelloController.java
@@ -3,6 +3,10 @@ package com.example.biblio;
 
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.VBox;
 
 import java.net.URL;
@@ -10,18 +14,49 @@ import java.util.ResourceBundle;
 
 public class HelloController implements Initializable{
     @FXML
-    private VBox global;
+    private Button btnValider;
 
     @FXML
-    private VBox test;
+    private Label labAuteur;
 
     @FXML
-    private VBox test2;
+    private Label labColonne;
+
+    @FXML
+    private Label labParution;
+
+    @FXML
+    private Label labRangee;
+
+    @FXML
+    private Label labResume;
+
+    @FXML
+    private Label labTitre;
+
+    @FXML
+    private TextField texteAuteur;
+
+    @FXML
+    private TextField texteColonne;
+
+    @FXML
+    private TextField texteParution;
+
+    @FXML
+    private TextField texteRangee;
+
+    @FXML
+    private TextArea texteResume;
+
+    @FXML
+    private TextField texteTitre;
+
 
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-       global.getChildren().removeAll(test);
+
 
     }
 }
